170a028dfde134b9bd3ed08234cdfb8891993ce6
LucasAndradeSilva <lucasandradesilva2019@gmail.com>
2023-08-01 15:15:01 -0300

    fisrt commit

---
+++ RedisCache/Controllers/WeatherForecastController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RedisCache.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get([FromServices]IDistributedCache cache)
+        {
+            var rng = new Random();
+            var forecastJson = cache.GetString("Forecast");
+
+            if (string.IsNullOrEmpty(forecastJson))
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+
+                forecastJson = JsonSerializer.Serialize(forecast);
+
+                var opcoesCache = new DistributedCacheEntryOptions();
+                opcoesCache.SetAbsoluteExpiration(TimeSpan.FromMinutes(1));
+
+                cache.SetString("Forecast", forecastJson, opcoesCache);
+                return forecast;
+            }
+            else
+            {
+                var forecast = JsonSerializer.Deserialize<WeatherForecast[]>(forecastJson);   
+                return forecast;
+            }
+                      
+        }
+    }
+}
